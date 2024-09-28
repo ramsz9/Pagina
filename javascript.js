d1198a47637373c5110849c7c07a065f2c459008
ram <hector.ara98@outlook.com>
2024-09-27 19:34:05 -0700

    Cambios en clase

---
+++ javascript.js
@@ -0,0 +1,7 @@
+//cadena.charAt(5) .- Muestra el caracter en la posicion 5
+// cadena.TOiNT()
+// cadena.split(" ") .- divide en elementos separados por el espacio en este caso
+
+// El autocompletado funciona con un TRIE 
+// Deconstruye cada elemento en nodos y un nodo especial define el final
+// 
